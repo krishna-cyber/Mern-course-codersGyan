cddd4252efc540e6ad7abf4438905d902930bd12
Krishna Tiwari <61178259+krishna-cyber@users.noreply.github.com>
2024-11-10 08:52:55 +0545

    add error handling middleware and update dependencies

--- authService/eslint.config.mjs
+++ authService/eslint.config.mjs
@@ -5,6 +5,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
       parserOptions: {
--- authService/src/app.ts
+++ authService/src/app.ts
@@ -1,9 +1,30 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import logger from "./config/logger";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const err = createHttpError(404, "You can't access this route");
+  throw err;
   res.send("Hello World!");
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message, err.statusCode);
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        message: err.message,
+        path: "",
+        location: "",
+      },
+    ],
+  });
 });
 
 export default app;
